970abd854a3f0fe8b2d6ef19c86d5d5d7136ca90
nocturnalcoders <christiantolie99@gmail.com>
2025-06-18 03:00:06 +0800

    change .gitignore

---
+++ target/classes/db/migration/V1.1.4__CREATE_TABLE_ACCOUNT_TYPE.sql
@@ -0,0 +1,28 @@
+-- V1__Create_Roles_Table.sql
+
+-- Drop table if it exists
+DROP TABLE IF EXISTS "ms-data-master-account".t_acct_type;
+
+-- Drop indexes if they exist
+DROP INDEX IF EXISTS idx_created_at;
+DROP INDEX IF EXISTS idx_updated_at;
+
+-- Create the table
+CREATE TABLE "ms-data-master-account".t_acct_type (
+                        id UUID PRIMARY KEY,
+                        name VARCHAR(255) NOT NULL,
+                        dscp VARCHAR(255) NOT NULL,
+                        version INTEGER,
+                        status VARCHAR(50),
+                        created_by VARCHAR(255),
+                        created_at TIMESTAMP,
+                        updated_by VARCHAR(255),
+                        updated_at TIMESTAMP,
+                        deleted_by VARCHAR(255),
+                        deleted_at TIMESTAMP
+);
+
+-- Create indexes for frequently queried columns
+CREATE INDEX idx_created_at ON "ms-data-master-account".t_roles(created_at);
+CREATE INDEX idx_updated_at ON "ms-data-master-account".t_roles(updated_at);
+
