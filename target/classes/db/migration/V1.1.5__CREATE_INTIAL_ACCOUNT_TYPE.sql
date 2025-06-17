db82fe5a3c99be0c39ca1eabc3e9a016b91add26
nocturnalcoders <christiantolie99@gmail.com>
2025-06-18 03:14:03 +0800

    change .gitignore

--- target/classes/db/migration/V1.1.5__CREATE_INTIAL_ACCOUNT_TYPE.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO t_acct_type ( id, name, dscp, version, status, created_by, created_at, updated_by, updated_at, deleted_at, deleted_by)
-VALUES
-    ('c4f8ac9a-8f87-4a67-9b79-70c3b944f3a5', 'CUSTOMER', 'Super Admin', 1, 'ACTIVE', 'system', '2024-11-24 22:39:16', NULL, NULL, NULL, NULL),
-    ('c4f8ac9a-8f87-4a67-9b79-70c3b944f3a4', 'SUPPLIER', 'Admin', 1, 'ACTIVE', 'system', '2024-11-24 22:39:18', NULL, NULL, NULL, NULL),
-    ('c4f8ac9a-8f87-4a67-9b79-70c3b944f3a3', 'CORPORATE', 'User', 1, 'ACTIVE', 'system', '2024-11-24 22:39:19', NULL, NULL, NULL, NULL)
-    ON CONFLICT (id) DO NOTHING;
