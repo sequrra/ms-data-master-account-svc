53baa89e69fef950fab77007886a3b8924d0aff2
nocturnalcoders <christiantolie99@gmail.com>
2025-06-19 19:35:28 +0800

    change .gitignore

--- src/main/java/com/ms/data/master/account/config/SecurityConfiguration.java
+++ src/main/java/com/ms/data/master/account/config/SecurityConfiguration.java
@@ -37,25 +37,6 @@ public class SecurityConfiguration {
         return getHttpSecurity6v(http).build();
     }
 
-
-    @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.addAllowedOriginPattern("*://localhost:*"); // Allows all origins with "localhost" and any port
-        corsConfiguration.addAllowedOriginPattern("http://139.162.36.148:3000"); // Allows all origins with "localhost" and any port
-        corsConfiguration.addAllowedOriginPattern("http://139.162.36.148:3001"); // Allows all origins with "localhost" and any port
-        corsConfiguration.addAllowedOriginPattern("https://admin.ugkslimousine.com"); // Allows all origins with "ugkslimousine.com"
-        corsConfiguration.addAllowedOriginPattern("https://ugkslimousine.com"); // Allows all origins with "ugkslimousine.com"
-        corsConfiguration.addAllowedHeader("*");
-        corsConfiguration.addAllowedMethod("*");
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfiguration);
-
-        return new CorsFilter(source);
-    }
-
     private HttpSecurity getHttpSecurity6v(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
