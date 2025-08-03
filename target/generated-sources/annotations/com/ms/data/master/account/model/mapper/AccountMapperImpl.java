f4b34e52297f4c6bcd4460733b3368ba2057c323
nocturnalcoders <christiantolie99@gmail.com>
2025-08-03 15:12:43 +0800

    adding target files

--- target/generated-sources/annotations/com/ms/data/master/account/model/mapper/AccountMapperImpl.java
+++ target/generated-sources/annotations/com/ms/data/master/account/model/mapper/AccountMapperImpl.java
@@ -8,7 +8,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-08-03T15:07:53+0800",
+    date = "2025-08-03T15:12:27+0800",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.11 (JetBrains s.r.o.)"
 )
 public class AccountMapperImpl implements AccountMapper {
